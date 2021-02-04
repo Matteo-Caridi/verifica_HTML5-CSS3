c1852927391c9dd7f0b280215f6608b7725d1d86
Matteo-Caridi <caridimatteo0@outlook.it>
2021-02-04 17:14:18 +0100

    creazione index crediti + css +js

---
+++ crediti/my.js
@@ -0,0 +1,10 @@
+// Botton up
+
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 200) {
+        $('.gotopbtn').fadeIn();
+    } else {
+        $('.gotopbtn').fadeOut();
+    }
+});
